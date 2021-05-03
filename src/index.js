9048af52aedabe2ae2bdae86c4040afea8ee1906
MatthiasChometon <matthias.chometon@ynov.com>
2021-05-03 18:19:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors({
 }));
 app.use(helmet());
 app.use(express.json({ limit: '50MB' }));
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT;
 
 // connection mongoose
 mongoose.Promise = global.Promise;
